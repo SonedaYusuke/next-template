297bfeb2b77ae1040d956cf1ec8da277f9456601
SonedaYusuke <yusuke.soneda@gmail.com>
2020-12-24 13:17:01 +0900

    recoil入れた

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import React from "react"
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
 import { AppProps } from "next/app"
 import "../styles/globals.css"
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+    )
 }
 
 export default App
